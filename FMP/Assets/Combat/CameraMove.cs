d088e10ae7317f3c1ee190616ed87f2cca00175f
Sarah Butlin <91489047+DragonKnight240@users.noreply.github.com>
2023-01-29 02:18:01 +0000

    Alt Cam controls

--- FMP/Assets/Combat/CameraMove.cs
+++ FMP/Assets/Combat/CameraMove.cs
@@ -11,6 +11,8 @@ public class CameraMove : MonoBehaviour
     public float SmoothSpeed;
     public float CloseEnough = 2;
     internal bool ShouldFollow = true;
+    internal Rigidbody RB;
+    public bool ButtonMovement = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,20 +27,32 @@ public class CameraMove : MonoBehaviour
         }
 
         Cam = FindObjectOfType<Camera>();
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ButtonMovement)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
+            RB.velocity = new Vector3((x * SmoothSpeed * Time.timeScale), 0, (z * SmoothSpeed * Time.timeScale));
+        }
     }
 
     private void LateUpdate()
     {
+        if(ButtonMovement)
+        {
+            return;
+        }
+
         if (FollowTarget)
         {
             Vector3 DesiredPosition = FollowTarget.position + offSet;
-            Vector3 SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed);
+            Vector3 SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed * Time.deltaTime);
 
             transform.position = SmoothPosition;
         }
