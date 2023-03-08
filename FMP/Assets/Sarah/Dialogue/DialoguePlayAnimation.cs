411b002c21eda0d4af8c061733b6b42cdfe74f30
Sarah Butlin <91489047+DragonKnight240@users.noreply.github.com>
2023-03-08 18:48:27 +0000

    Animations on Dialogue

--- FMP/Assets/Sarah/Dialogue/DialoguePlayAnimation.cs
+++ FMP/Assets/Sarah/Dialogue/DialoguePlayAnimation.cs
@@ -6,15 +6,16 @@ public class DialoguePlayAnimation : MonoBehaviour
 {
     public Dialogue Line;
     public Animator Animator;
-    internal AnimationController NewAnimControl;
-    internal AnimationController OGAnimControl;
     public string TriggerName;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Animator = GetComponent<Animator>();
+        if (Animator == null)
+        {
+            Animator = GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
