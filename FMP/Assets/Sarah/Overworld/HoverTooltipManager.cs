c9f92b0f8f9bdd64533d39a457a9144269d69d33
Sarah Butlin <91489047+DragonKnight240@users.noreply.github.com>
2023-03-15 01:25:23 +0000

    Hover Tooltips

---
+++ FMP/Assets/Sarah/Overworld/HoverTooltipManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+
+public class HoverTooltipManager : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    public RectTransform tipObject;
+
+    public static Action<string, Vector2> OnMouseOver;
+    public static Action OnMouseLoseFocus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideTip();
+    }
+
+    void ShowTip(string tip, Vector2 MousePos)
+    {
+        Text.text = tip;
+        tipObject.sizeDelta = new Vector2(Text.preferredWidth > 200 ? 200 : Text.preferredWidth, Text.preferredHeight);
+
+        tipObject.gameObject.SetActive(true);
+        tipObject.transform.position = new Vector2(MousePos.x + tipObject.sizeDelta.x * 2, MousePos.y);
+    }
+
+    void HideTip()
+    {
+        Text.text = default;
+        tipObject.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        OnMouseOver += ShowTip;
+        OnMouseLoseFocus += HideTip;
+    }
+
+    private void OnDisable()
+    {
+        OnMouseOver -= ShowTip;
+        OnMouseLoseFocus -= HideTip;
+    }
+}
